3db324a574ebe478095efd2493d14128d1b25629
Samuel Prada <jspradac@gmail.com>
2018-11-07 19:33:19 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0 
  selection = []
  while i < collection.length do 
-   if yield array[i]
+   if yield array[i] == true 
      selection.push(array[i])
    end 
    i += 1 
